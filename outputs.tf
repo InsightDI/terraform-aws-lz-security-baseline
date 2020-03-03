9b186c2f9e2f52aa769ac727d4c789d8e08ecff9
Michael Meadows <michaelmeadows@gmail.com>
2020-03-03 16:30:50 -0500

    Updated outputs to include a description

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "aws_guardduty_detector_id" {
-  value = aws_guardduty_detector.guardduty.id
+  description = "The ID of the Guardduty detector created for this account."
+  value       = aws_guardduty_detector.guardduty.id
 }
